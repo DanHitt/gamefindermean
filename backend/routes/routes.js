a90a9ffc76a0f895976ec3146f64459f924ec1f5
Dan Hitt <d.hitt424@gmail.com>
2016-10-04 11:09:58 -0600

    add login

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,41 @@
-var express = require('express'),
-    routes = express.Router()
+var express = require('express')
+var Account = require('../models/account');
+var passport = require('passport');
+var router = express.Router()
 
-routes.route("/")
-.get()
+router.get('/', function (req, res){
+    res.render('index', { user : req.user});
+});
+router.get('/register', function(req,res) {
+    res.render('regiser', { });
+});
+router.post('/register', function(req,res) {
+    Account.register(new Account({ username:req.body.username }), req.body.password, function(err,account) {
+        if (err) {
+            return res.render('register', {account : account });
+        }
+        
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/');
+        });
+    });
+});
 
-module.exports = routes;
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+});
+
+module.exports = router;
